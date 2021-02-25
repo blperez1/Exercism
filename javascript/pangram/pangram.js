820497094bc81c7396c4e74699985874ef203c8b
Unknown <blossomperez18@gmail.com>
2021-02-24 23:47:26 -0600

    Complete challenge

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,7 +2,9 @@
 // This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPangram = (str) => {
+  return [...alphabet]
+          .every(char => str.toLowerCase().indexOf(char) > -1)
 };
